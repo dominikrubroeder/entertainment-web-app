5d1bfa84e37b0b44a5bf927be9c8718e93a52d03
Dominik <dominik.rubroeder@icloud.com>
2022-07-29 13:44:18 +0200

    Set and clean up initial project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'app-primary-red': 'hsl(0°, 97%, 63%)',
+        'app-blue-300': 'hsl(223°, 23%, 46%)',
+        'app-blue-800': 'hsl(223°, 36%, 14%)',
+        'app-blue-900': 'hsl(223°, 30%, 9%)',
+      },
+    },
+  },
+  plugins: [],
+};
