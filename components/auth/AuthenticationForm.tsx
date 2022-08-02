26d33cbf1e6fd1ae93562c41011ca2f100c6a2b6
Dominik <dominik.rubroeder@icloud.com>
2022-08-02 14:07:19 +0200

    Add LoadingSpinner

--- components/auth/AuthenticationForm.tsx
+++ components/auth/AuthenticationForm.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import LoadingSpinner from '../LoadingSpinner';
 
 interface AuthenticationFormProps {
   isLogin: boolean;
 }
 
 const AuthenticationForm: React.FC<AuthenticationFormProps> = ({ isLogin }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    // validate...
+
+    setTimeout(() => {
+      setIsLoading(false);
+      router.replace('/');
+    }, 2000);
+  };
+
   const inputClasses =
     'p-4 bg-transparent border-b-2 border-b-app-blue-300 focus:border-b-white focus:outline-none';
 
+  if (isLoading)
+    return (
+      <div className="animate-bounce m-auto my-10">
+        <LoadingSpinner className="text-app-primary-red" />
+      </div>
+    );
+
   return (
-    <form className="grid gap-4">
+    <form className="grid gap-4" onSubmit={handleSubmit}>
       <input
         placeholder="Email address"
         type="email"
@@ -27,7 +52,8 @@ const AuthenticationForm: React.FC<AuthenticationFormProps> = ({ isLogin }) => {
       )}
 
       <button className="bg-app-primary-red text-white p-4 rounded-xl">
-        {isLogin ? 'Login to your account' : 'Create account'}
+        {isLoading && 'Loading...'}
+        {!isLoading && isLogin ? 'Login to your account' : 'Create account'}
       </button>
     </form>
   );
