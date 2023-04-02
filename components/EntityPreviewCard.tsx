a9e207db232682656371b06492af3f4087789f0c
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-04-02 13:33:24 +0200

    Fix HTML empty Button text warning

--- components/EntityPreviewCard.tsx
+++ components/EntityPreviewCard.tsx
@@ -23,17 +23,17 @@ const EntityPreviewCard: React.FC<EntityPreviewCardProps> = ({
   const id = data.title + data.year;
 
   const bookmark = (
-    <button
-      className="absolute top-4 right-4 w-8 h-8 rounded-full bg-app-blue-900/50 z-50 flex items-center justify-center cursor-pointer hover:animate-scale"
+    <div
+      className='absolute top-4 right-4 w-8 h-8 rounded-full bg-app-blue-900/50 z-50 flex items-center justify-center cursor-pointer hover:animate-scale'
       onClick={() => entityCtx?.toggleBookmarked(id)}
     >
       {data.isBookmarked ? <IconBookmarkFill /> : <IconBookmarkEmpty />}
-    </button>
+    </div>
   );
 
   const playOverlay = (
-    <div className="absolute inset-0 bg-black/50 rounded-xl flex items-center justify-center w-full h-full opacity-0 invisible z-30 group-hover:opacity-100 group-hover:visible">
-      <button className="flex gap-2 items-center justify-center rounded-full bg-white/25 text-lg font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-2 z-40">
+    <div className='absolute inset-0 bg-black/50 rounded-xl flex items-center justify-center w-full h-full opacity-0 invisible z-30 group-hover:opacity-100 group-hover:visible'>
+      <button className='flex gap-2 items-center justify-center rounded-full bg-white/25 text-lg font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-2 z-40'>
         <IconPlay /> Play
       </button>
     </div>
@@ -42,14 +42,14 @@ const EntityPreviewCard: React.FC<EntityPreviewCardProps> = ({
   if (data.isTrending && trendingIsShown)
     return (
       <div
-        className="group relative flex justify-end flex-col gap-2 w-[29.375rem] h-[14.375rem] rounded-xl cursor-pointer animate-fadeUp"
+        className='group relative flex justify-end flex-col gap-2 w-[29.375rem] h-[14.375rem] rounded-xl cursor-pointer animate-fadeUp'
         style={{ animationDelay: `${1000 * index}ms` }}
       >
-        <div className="z-10 bg-gradient-to-b from-black/0 to-black/75 p-6 rounded-b-xl">
-          <ul className="flex items-center gap-2 text-xs">
+        <div className='z-10 bg-gradient-to-b from-black/0 to-black/75 p-6 rounded-b-xl'>
+          <ul className='flex items-center gap-2 text-xs'>
             <li>{data.year}</li>
-            <li className="w-[0.1875rem] h-[0.1875rem] rounded-full bg-white"></li>
-            <li className="flex items-center gap-2">
+            <li className='w-[0.1875rem] h-[0.1875rem] rounded-full bg-white'></li>
+            <li className='flex items-center gap-2'>
               {data.category === EntityCategory.movie ? (
                 <IconCategoryMovie />
               ) : (
@@ -57,17 +57,17 @@ const EntityPreviewCard: React.FC<EntityPreviewCardProps> = ({
               )}
               {data.category}
             </li>
-            <li className="w-[0.1875rem] h-[0.1875rem] rounded-full bg-white"></li>
+            <li className='w-[0.1875rem] h-[0.1875rem] rounded-full bg-white'></li>
             <li>{data.rating}</li>
           </ul>
-          <h3 className="text-2xl font-bold">{data.title}</h3>
+          <h3 className='text-2xl font-bold'>{data.title}</h3>
         </div>
 
         <Image
           src={data.thumbnail.trending!.large}
-          layout="fill"
+          layout='fill'
           alt={`${data.title} thumbnail`}
-          className="rounded-xl"
+          className='rounded-xl'
           priority={entityCtx!.trending[0].title === data.title}
         />
 
@@ -79,28 +79,28 @@ const EntityPreviewCard: React.FC<EntityPreviewCardProps> = ({
 
   return (
     <div
-      className="group cursor-pointer opacity-0 invisible animate-fadeUp"
+      className='group cursor-pointer opacity-0 invisible animate-fadeUp'
       style={{ animationDelay: `${100 * index}ms` }}
     >
-      <div className="relative">
+      <div className='relative'>
         {playOverlay}
 
         <Image
           src={data.thumbnail.regular.large}
-          layout="responsive"
+          layout='responsive'
           alt={`${data.title} thumbnail`}
-          className="rounded-xl"
+          className='rounded-xl'
           width={280}
           height={174}
         />
         {bookmark}
       </div>
 
-      <div className="mt-2">
-        <ul className="flex items-center gap-2 text-xs">
+      <div className='mt-2'>
+        <ul className='flex items-center gap-2 text-xs'>
           <li>{data.year}</li>
-          <li className="w-[0.1875rem] h-[0.1875rem] rounded-full bg-white"></li>
-          <li className="flex items-center gap-2">
+          <li className='w-[0.1875rem] h-[0.1875rem] rounded-full bg-white'></li>
+          <li className='flex items-center gap-2'>
             {data.category === EntityCategory.movie ? (
               <IconCategoryMovie />
             ) : (
@@ -108,10 +108,10 @@ const EntityPreviewCard: React.FC<EntityPreviewCardProps> = ({
             )}
             {data.category}
           </li>
-          <li className="w-[0.1875rem] h-[0.1875rem] rounded-full bg-white"></li>
+          <li className='w-[0.1875rem] h-[0.1875rem] rounded-full bg-white'></li>
           <li>{data.rating}</li>
         </ul>
-        <h3 className="text-lg font-bold">{data.title}</h3>
+        <h3 className='text-lg font-bold'>{data.title}</h3>
       </div>
     </div>
   );
